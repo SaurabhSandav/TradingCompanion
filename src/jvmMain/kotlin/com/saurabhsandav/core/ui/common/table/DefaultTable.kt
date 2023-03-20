14fb5241ff9fb9c8470c21b878a9679a5df4fb32
Saurabh Sandav <justmailsaurabh@gmail.com>
2023-03-20 19:39:52 +0530

    Make DefaultTableRow clickable

--- src/jvmMain/kotlin/com/saurabhsandav/core/ui/common/table/DefaultTable.kt
+++ src/jvmMain/kotlin/com/saurabhsandav/core/ui/common/table/DefaultTable.kt
@@ -1,20 +1,14 @@
 package com.saurabhsandav.core.ui.common.table
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.PointerEventType
-import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.unit.dp
-import com.saurabhsandav.core.ui.common.state
 
 @Composable
 fun <T> DefaultTableHeader(
@@ -40,30 +34,20 @@ fun <T> DefaultTableRow(
     item: T,
     schema: TableSchema<T>,
     modifier: Modifier = Modifier,
+    onClick: () -> Unit = {},
 ) {
 
-    var rowActive by state { false }
-
-    Surface(
-        color = when {
-            rowActive -> MaterialTheme.colorScheme.surfaceVariant
-            else -> MaterialTheme.colorScheme.surface
-        }
+    Row(
+        modifier = Modifier
+            .clickable(onClick = onClick)
+            .padding(8.dp)
+            .then(modifier),
+        verticalAlignment = Alignment.CenterVertically,
     ) {
 
-        Row(
-            modifier = Modifier
-                .onPointerEvent(PointerEventType.Enter) { rowActive = true }
-                .onPointerEvent(PointerEventType.Exit) { rowActive = false }
-                .padding(8.dp)
-                .then(modifier),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-
-            schema.columns.forEach { column ->
-                Box(Modifier.weight(column.span)) {
-                    column.content(item)
-                }
+        schema.columns.forEach { column ->
+            Box(Modifier.weight(column.span)) {
+                column.content(item)
             }
         }
     }
