da305a7928664ee6c2186fbfca5ac952e3a8bdfa
Saurabh Sandav <justmailsaurabh@gmail.com>
2023-08-22 13:56:05 +0530

    Fix: Paged charts not rendered when not selected

--- src/jvmMain/resources/charts_page/arrangement_pagedchart.js
+++ src/jvmMain/resources/charts_page/arrangement_pagedchart.js
@@ -5,7 +5,7 @@ function preparePagedChartContainer(chartId) {
   chartDiv.id = chartId;
   chartDiv.className = 'tabcontent';
   document.body.appendChild(chartDiv);
-  chartDiv.style.display = "none";
+  chartDiv.style.visibility = "hidden";
   chartDiv.style.position = 'absolute';
   chartDiv.style.height = '100%';
   chartDiv.style.width = '100%';
@@ -38,10 +38,10 @@ function showPagedChart(chartId) {
   var tabcontent = document.getElementsByClassName("tabcontent");
 
   for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+    tabcontent[i].style.visibility = "hidden";
   }
 
-  document.getElementById(chartId).style.display = "block";
+  document.getElementById(chartId).style.visibility = "visible";
 }
 
 function setPagedLegendTexts(chartId, list) {
