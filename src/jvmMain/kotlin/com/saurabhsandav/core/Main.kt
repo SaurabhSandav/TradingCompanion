665b90ee4202305015a577b179547733efde767f
Saurabh Sandav <justmailsaurabh@gmail.com>
2023-09-05 13:44:58 +0530

    Add Exit confirmation dialog

--- src/jvmMain/kotlin/com/saurabhsandav/core/Main.kt
+++ src/jvmMain/kotlin/com/saurabhsandav/core/Main.kt
@@ -1,41 +1,24 @@
 package com.saurabhsandav.core
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import com.saurabhsandav.core.ui.common.app.AppWindow
 import com.saurabhsandav.core.ui.common.app.rememberAppWindowState
+import com.saurabhsandav.core.ui.common.state
 import com.saurabhsandav.core.ui.landing.LandingScreen
 import com.saurabhsandav.core.ui.theme.AppTheme
 import com.saurabhsandav.core.utils.PrefDefaults
 import com.saurabhsandav.core.utils.PrefKeys
 
-@Composable
-@Preview
-internal fun App() {
-
-    val appModule = LocalAppModule.current
-    val useDarkTheme by remember {
-        appModule.appPrefs.getBooleanFlow(PrefKeys.DarkModeEnabled, PrefDefaults.DarkModeEnabled)
-    }.collectAsState(PrefDefaults.DarkModeEnabled)
-
-    AppTheme(useDarkTheme = useDarkTheme) {
-
-        Surface {
-            LandingScreen()
-        }
-    }
-}
-
 fun main() = application {
 
-    val windowState = rememberAppWindowState(
-        windowState = rememberWindowState(placement = WindowPlacement.Maximized),
-        defaultTitle = "Trading Companion",
-    )
     val appModule = remember { AppModule() }
 
     val densityFraction by appModule.appPrefs.getFloatFlow(PrefKeys.DensityFraction, PrefDefaults.DensityFraction)
@@ -46,14 +29,71 @@ fun main() = application {
         LocalAppModule provides appModule,
     ) {
 
-        AppWindow(
-            onCloseRequest = ::exitApplication,
-            state = windowState,
-        ) {
+        App(::exitApplication)
+    }
+}
 
-            App()
+@Composable
+@Preview
+internal fun App(onCloseRequest: () -> Unit) {
+
+    val windowState = rememberAppWindowState(
+        windowState = rememberWindowState(placement = WindowPlacement.Maximized),
+        defaultTitle = "Trading Companion",
+    )
+
+    var showExitConfirmationDialog by state { false }
+
+    AppWindow(
+        onCloseRequest = { showExitConfirmationDialog = true },
+        state = windowState,
+    ) {
+
+        val appModule = LocalAppModule.current
+        val useDarkTheme by remember {
+            appModule.appPrefs.getBooleanFlow(PrefKeys.DarkModeEnabled, PrefDefaults.DarkModeEnabled)
+        }.collectAsState(PrefDefaults.DarkModeEnabled)
+
+        AppTheme(useDarkTheme = useDarkTheme) {
+
+            Surface {
+
+                LandingScreen()
+
+                if (showExitConfirmationDialog) {
+
+                    ExitConfirmationDialog(
+                        onDismiss = { showExitConfirmationDialog = false },
+                        onConfirm = onCloseRequest,
+                    )
+                }
+            }
         }
     }
+}
+
+@Composable
+private fun ExitConfirmationDialog(
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit,
+) {
+
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        text = {
+            Text("Are you sure you want to exit?")
+        },
+        confirmButton = {
+            Button(onClick = onConfirm) {
+                Text("Yes")
+            }
+        },
+        dismissButton = {
+            Button(onClick = onDismiss) {
+                Text("No")
+            }
+        },
+    )
 }
 
 internal val LocalDensityFraction = staticCompositionLocalOf { PrefDefaults.DensityFraction }
