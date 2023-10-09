53764b757fbc031526f79029824a2307e9c62b57
Saurabh Sandav <justmailsaurabh@gmail.com>
2023-10-09 18:43:45 +0530

    FyersRateLimiter: Fixes and Improvements
    No longer tracks day's requests

--- src/jvmMain/kotlin/com/saurabhsandav/core/fyers_api/FyersRateLimiter.kt
+++ src/jvmMain/kotlin/com/saurabhsandav/core/fyers_api/FyersRateLimiter.kt
@@ -1,33 +1,21 @@
 package com.saurabhsandav.core.fyers_api
 
 import kotlinx.coroutines.delay
-import kotlinx.datetime.*
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
 internal class FyersRateLimiter {
 
-    private var trackedDay = today()
-    private var requestsToday = 0
-
-    private var trackedMinute = currentMinute()
+    private var trackedMinute = Instant.DISTANT_PAST
     private var requestsInMinute = 0
 
-    private var trackedSecond = currentSecond()
+    private var trackedSecond = Instant.DISTANT_PAST
     private var requestsInSecond = 0
 
     suspend fun limit() {
-
-        // Day (10000 per day)
-        when (trackedDay) {
-            // If tracked day is today, check if requests exceed daily rate limit
-            today() -> if (requestsToday > 10000) error("Fyers: Rate Limit exceeded for the day")
-            // Else, reset day tracking
-            else -> {
-                trackedDay = today()
-                requestsToday = 0
-            }
-        }
 
         // Minutes (200 per minute)
         val currentMinute = currentMinute()
@@ -35,13 +23,13 @@ internal class FyersRateLimiter {
 
         if (durationSinceTrackedMinute <= 1.minutes) {
             // If tracked minute is still going, check if requests exceed minute rate limit
-            if (requestsInMinute > 200) {
+            if (requestsInMinute >= 200) {
                 // If exceeds, suspend for the rest of the minute
-                delay(1.minutes - durationSinceTrackedMinute)
+                delay(1.minutes - durationSinceTrackedMinute + 1.seconds)
             }
         } else {
             // Else, reset minute tracking
-            trackedMinute = currentMinute()
+            trackedMinute = currentMinute
             requestsInMinute = 0
         }
 
@@ -51,22 +39,19 @@ internal class FyersRateLimiter {
 
         if (durationSinceTrackedSecond <= 1.seconds) {
             // If tracked second is still going, check if requests exceed minute rate limit
-            if (requestsInSecond > 10) {
+            if (requestsInSecond >= 10) {
                 // If exceeds, suspend for the rest of the second
-                delay(1.seconds - durationSinceTrackedSecond)
+                delay(1.seconds - durationSinceTrackedSecond + 10.milliseconds)
             }
         } else {
             // Else, reset second tracking
-            trackedSecond = currentSecond()
+            trackedSecond = currentSecond
             requestsInSecond = 0
         }
 
-        requestsToday++
         requestsInSecond++
         requestsInMinute++
     }
-
-    private fun today(): LocalDate = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date
 
     private fun currentMinute(): Instant = Clock.System.now()
 
