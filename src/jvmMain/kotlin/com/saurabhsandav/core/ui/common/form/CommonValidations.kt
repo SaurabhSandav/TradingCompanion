5dc933a76c167553bd5738c5da35a5404b8a9375
Saurabh Sandav <justmailsaurabh@gmail.com>
2023-10-17 00:34:14 +0530

    Delete Form (validation) v1

--- src/jvmMain/kotlin/com/saurabhsandav/core/ui/common/form/CommonValidations.kt
+++
@@ -1,5 +0,0 @@
-package com.saurabhsandav.core.ui.common.form
-
-val IsBigDecimal = Validation<String>("Not a valid number") { it.toBigDecimalOrNull() != null }
-
-val IsInt = Validation<String>("Not a valid integer") { it.toIntOrNull() != null }
