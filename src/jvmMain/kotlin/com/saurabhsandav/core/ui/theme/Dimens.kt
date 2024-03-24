187636aaad0b62a054c53afb9b37bcc46f0bd6ac
Saurabh Sandav <justmailsaurabh@gmail.com>
2024-03-24 13:37:27 +0530

    Add MaterialTheme.dimens

---
+++ src/jvmMain/kotlin/com/saurabhsandav/core/ui/theme/Dimens.kt
@@ -0,0 +1,9 @@
+package com.saurabhsandav.core.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+
+@Suppress("UnusedReceiverParameter")
+val MaterialTheme.dimens: Dimens
+    get() = Dimens
+
+object Dimens
