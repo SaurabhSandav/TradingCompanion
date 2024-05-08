ee8093ec6b064086fc3d08efed12f0559f0881e7
Saurabh Sandav <justmailsaurabh@gmail.com>
2024-05-08 13:29:51 +0530

    Reimplement PNLStudy

--- src/jvmMain/kotlin/com/saurabhsandav/core/ui/stats/studies/PNLStudy.kt
+++ src/jvmMain/kotlin/com/saurabhsandav/core/ui/stats/studies/PNLStudy.kt
@@ -1,119 +1,230 @@
 package com.saurabhsandav.core.ui.stats.studies
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import app.cash.paging.Pager
+import app.cash.paging.PagingConfig
+import app.cash.paging.PagingData
+import app.cash.paging.compose.collectAsLazyPagingItems
+import app.cash.paging.compose.itemKey
+import app.cash.paging.map
 import com.saurabhsandav.core.trades.TradingProfiles
-import com.saurabhsandav.core.trades.brokerageAtExit
 import com.saurabhsandav.core.trades.model.ProfileId
+import com.saurabhsandav.core.trades.model.TradeFilter
+import com.saurabhsandav.core.trades.model.TradeId
+import com.saurabhsandav.core.trades.model.TradeSort
 import com.saurabhsandav.core.trades.rValueAt
 import com.saurabhsandav.core.ui.common.AppColor
-import com.saurabhsandav.core.ui.common.table.TableSchema
-import com.saurabhsandav.core.ui.common.table.addColumn
-import com.saurabhsandav.core.ui.common.table.addColumnText
-import com.saurabhsandav.core.ui.common.table.tableSchema
+import com.saurabhsandav.core.ui.common.TradeDateTimeFormatter
+import com.saurabhsandav.core.ui.common.table2.*
+import com.saurabhsandav.core.ui.common.table2.TableCell.Width.Fixed
+import com.saurabhsandav.core.ui.common.table2.TableCell.Width.Weight
 import com.saurabhsandav.core.utils.emitInto
 import com.saurabhsandav.core.utils.format
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 import java.math.BigDecimal
-import java.time.format.DateTimeFormatter
-import java.time.format.FormatStyle
 
 internal class PNLStudy(
     profileId: ProfileId,
     tradingProfiles: TradingProfiles,
-) : TableStudy<PNLStudy.Model>() {
+) : Study {
 
-    override val schema: TableSchema<Model> = tableSchema {
-        addColumnText("Ticker") { it.ticker }
-        addColumnText("Quantity") { it.quantity }
-        addColumn("Side") {
-            Text(it.side, color = if (it.side == "LONG") AppColor.ProfitGreen else AppColor.LossRed)
+    @Composable
+    override fun render() {
+
+        val items = data.collectAsLazyPagingItems()
+
+        LazyTable(
+            modifier = Modifier.fillMaxSize(),
+            headerContent = {
+
+                Schema.SimpleHeader {
+                    id.text { "ID" }
+                    ticker.text { "Ticker" }
+                    side.text { "Side" }
+                    quantity.text { "Quantity" }
+                    avgEntry.text { "Avg. Entry" }
+                    avgExit.text { "Avg. Exit" }
+                    entryTime.text { "Entry Time" }
+                    duration.text { "Duration" }
+                    stop.text { "Stop" }
+                    target.text { "Target" }
+                    pnl.text { "PNL" }
+                    netPnl.text { "Net PNL" }
+                    fees.text { "Fees" }
+                    rValue.text { "R" }
+                }
+            },
+        ) {
+
+            items(
+                count = items.itemCount,
+                key = items.itemKey { it.id }
+            ) { index ->
+
+                val item = items[index]!!
+                val generated = item.generated.collectAsState(null).value
+
+                Schema.SimpleRow {
+                    id.text { item.id.value.toString() }
+                    ticker.text { item.ticker }
+                    side {
+                        Text(item.side, color = if (item.side == "LONG") AppColor.ProfitGreen else AppColor.LossRed)
+                    }
+                    quantity.text { item.quantity }
+                    avgEntry.text { item.entry }
+                    avgExit.text { item.exit }
+                    entryTime.text { item.entryTime }
+                    duration.text { item.duration }
+
+                    if (generated != null) {
+                        stop.text { generated.stop }
+                        target.text { generated.target }
+                    }
+
+                    pnl {
+                        Text(
+                            item.pnl,
+                            color = if (item.isProfitable) AppColor.ProfitGreen else AppColor.LossRed,
+                        )
+                    }
+                    netPnl {
+                        Text(
+                            item.netPnl,
+                            color = if (item.isProfitable) AppColor.ProfitGreen else AppColor.LossRed,
+                        )
+                    }
+                    fees.text { item.fees }
+
+                    if (generated != null) {
+                        rValue.text { generated.rValue }
+                    }
+                }
+
+                HorizontalDivider()
+            }
         }
-        addColumnText("Entry") { it.entry }
-        addColumnText("Stop") { it.stop }
-        addColumnText("Duration") { it.duration }
-        addColumnText("Target") { it.target }
-        addColumnText("Exit") { it.exit }
-        addColumn("PNL") {
-            Text(it.pnl, color = if (it.isProfitable) AppColor.ProfitGreen else AppColor.LossRed)
-        }
-        addColumn("Net PNL") {
-            Text(it.netPnl, color = if (it.isNetProfitable) AppColor.ProfitGreen else AppColor.LossRed)
-        }
-        addColumnText("Fees") { it.fees }
-        addColumnText("R") { it.rValue }
     }
 
-    override val data: Flow<List<Model>> = flow {
+    private val data: Flow<PagingData<Item>> = flow {
+
+        val pagingConfig = PagingConfig(
+            pageSize = 70,
+            enablePlaceholders = false,
+            maxSize = 300,
+        )
 
         val tradesRepo = tradingProfiles.getRecord(profileId).trades
 
-        tradesRepo.allTrades.flatMapLatest { trades ->
+        Pager(
+            config = pagingConfig,
+            pagingSourceFactory = {
 
-            val closedTrades = trades.filter { it.isClosed }
-            val closedTradesIds = closedTrades.map { it.id }
+                tradesRepo.getFilteredPagingSource(
+                    filter = TradeFilter(isClosed = true),
+                    sort = TradeSort.EntryDesc,
+                )
+            },
+        ).flow.map { pagingData ->
 
-            combine(
-                tradesRepo.getPrimaryStops(closedTradesIds),
-                tradesRepo.getPrimaryTargets(closedTradesIds),
-            ) { stops, targets ->
+            pagingData.map { trade ->
 
-                closedTrades.map { trade ->
+                val durationStr = run {
 
-                    val stop = stops.find { it.tradeId == trade.id }
-                    val target = targets.find { it.tradeId == trade.id }
+                    val durationSeconds = (trade.exitTimestamp!! - trade.entryTimestamp).inWholeSeconds
 
-                    val brokerage = trade.brokerageAtExit()!!
-                    val pnlBD = brokerage.pnl
-                    val netPnlBD = brokerage.netPNL
-
-                    val rValue = stop?.let { trade.rValueAt(pnl = pnlBD, stop = it) }
-
-                    val entryLDT = trade.entryTimestamp.toLocalDateTime(TimeZone.currentSystemDefault())
-                    val exitLDT = trade.exitTimestamp?.toLocalDateTime(TimeZone.currentSystemDefault())
-                    val day = DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG).format(entryLDT)
-
-                    Model(
-                        ticker = trade.ticker,
-                        quantity = trade.quantity.toPlainString(),
-                        side = trade.side.strValue.uppercase(),
-                        entry = trade.averageEntry.toPlainString(),
-                        stop = stop?.price?.toPlainString() ?: "NA",
-                        duration = "$day\n${entryLDT.time} ->\n${exitLDT?.time}",
-                        target = target?.price?.toPlainString() ?: "NA",
-                        exit = trade.averageExit!!.toPlainString(),
-                        pnl = pnlBD.toPlainString(),
-                        isProfitable = pnlBD > BigDecimal.ZERO,
-                        netPnl = netPnlBD.toPlainString(),
-                        isNetProfitable = netPnlBD > BigDecimal.ZERO,
-                        fees = (pnlBD - netPnlBD).toPlainString(),
-                        rValue = rValue?.let { "${it}R" }.orEmpty(),
+                    "%02d:%02d:%02d".format(
+                        durationSeconds / 3600,
+                        (durationSeconds % 3600) / 60,
+                        durationSeconds % 60,
                     )
                 }
+
+                Item(
+                    id = trade.id,
+                    ticker = trade.ticker,
+                    side = trade.side.strValue.uppercase(),
+                    quantity = trade.quantity.toPlainString(),
+                    entry = trade.averageEntry.toPlainString(),
+                    exit = trade.averageExit!!.toPlainString(),
+                    entryTime = TradeDateTimeFormatter.format(
+                        ldt = trade.entryTimestamp.toLocalDateTime(TimeZone.currentSystemDefault())
+                    ),
+                    duration = durationStr,
+                    pnl = trade.pnl.toPlainString(),
+                    isProfitable = trade.pnl > BigDecimal.ZERO,
+                    netPnl = trade.netPnl.toPlainString(),
+                    isNetProfitable = trade.netPnl > BigDecimal.ZERO,
+                    fees = (trade.pnl - trade.netPnl).toPlainString(),
+                    generated = combine(
+                        tradesRepo.getPrimaryStop(trade.id),
+                        tradesRepo.getPrimaryTarget(trade.id),
+                    ) { stop, target ->
+
+                        val rValue = stop?.let { trade.rValueAt(pnl = trade.pnl, stop = it) }
+
+                        Generated(
+                            stop = stop?.price?.toPlainString() ?: "NA",
+                            target = target?.price?.toPlainString() ?: "NA",
+                            rValue = rValue?.let { "${it}R" }.orEmpty(),
+                        )
+                    },
+                )
             }
+
         }.emitInto(this)
     }
 
-    data class Model(
+    private data class Item(
+        val id: TradeId,
         val ticker: String,
-        val quantity: String,
         val side: String,
+        val quantity: String,
         val entry: String,
-        val stop: String,
-        val duration: String,
-        val target: String,
         val exit: String,
+        val entryTime: String,
+        val duration: String,
         val pnl: String,
         val isProfitable: Boolean,
         val netPnl: String,
         val isNetProfitable: Boolean,
         val fees: String,
+        val generated: Flow<Generated>,
+    )
+
+    private data class Generated(
+        val stop: String,
+        val target: String,
         val rValue: String,
     )
+
+    private object Schema : TableSchema() {
+
+        val id = cell(Fixed(48.dp))
+        val ticker = cell(Weight(1.7F))
+        val side = cell()
+        val quantity = cell()
+        val avgEntry = cell()
+        val avgExit = cell()
+        val entryTime = cell(Weight(2.2F))
+        val duration = cell(Weight(1.5F))
+        val stop = cell()
+        val target = cell()
+        val pnl = cell()
+        val netPnl = cell()
+        val fees = cell()
+        val rValue = cell()
+    }
 
     class Factory(
         private val profileId: ProfileId,
