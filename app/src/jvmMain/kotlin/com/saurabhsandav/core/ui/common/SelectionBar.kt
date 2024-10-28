b9e0bcfb3719f6f1d342b66a7a22c66a6a0acf1f
Saurabh Sandav <justmailsaurabh@gmail.com>
2024-10-28 18:27:01 +0530

    Add SelectionBar UI component

---
+++ app/src/jvmMain/kotlin/com/saurabhsandav/core/ui/common/SelectionBar.kt
@@ -0,0 +1,86 @@
+package com.saurabhsandav.core.ui.common
+
+import androidx.compose.animation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import com.saurabhsandav.core.ui.theme.dimens
+
+@Composable
+internal fun SelectionBar(
+    selectionManager: SelectionManager<*>,
+    content: @Composable SelectionBarScope.() -> Unit,
+) {
+
+    AnimatedVisibility(
+        visible = selectionManager.selection.isNotEmpty(),
+        enter = fadeIn() + expandVertically(),
+        exit = shrinkVertically() + fadeOut(),
+    ) {
+
+        Surface(color = MaterialTheme.colorScheme.tertiaryContainer) {
+
+            Row(
+                modifier = Modifier.height(48.dp).fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+
+                IconButton(
+                    onClick = selectionManager::clear,
+                    content = {
+                        Icon(Icons.Default.Close, contentDescription = "Clear selection")
+                    },
+                )
+
+                Spacer(Modifier.width(MaterialTheme.dimens.rowHorizontalSpacing))
+
+
+                Text(
+                    modifier = Modifier.animateContentSize(),
+                    text = kotlin.run {
+                        val size = selectionManager.selection.size
+                        "$size ${if (size == 1) "item" else "items"} selected"
+                    },
+                )
+
+                Spacer(Modifier.width(MaterialTheme.dimens.rowHorizontalSpacing))
+
+                VerticalDivider()
+
+                SelectionBarScopeImpl.content()
+            }
+        }
+    }
+}
+
+interface SelectionBarScope {
+
+    @Composable
+    fun Item(
+        onClick: () -> Unit,
+        text: String,
+    )
+}
+
+private object SelectionBarScopeImpl : SelectionBarScope {
+
+    @Composable
+    override fun Item(
+        onClick: () -> Unit,
+        text: String,
+    ) {
+
+        TextButton(
+            modifier = Modifier.fillMaxHeight(),
+            onClick = onClick,
+            shape = RectangleShape,
+            content = { Text(text) },
+        )
+    }
+}
--- app/src/jvmMain/kotlin/com/saurabhsandav/core/ui/tradeexecutions/ui/TradeExecutionsSelectionBar.kt
+++ app/src/jvmMain/kotlin/com/saurabhsandav/core/ui/tradeexecutions/ui/TradeExecutionsSelectionBar.kt
@@ -1,22 +1,10 @@
 package com.saurabhsandav.core.ui.tradeexecutions.ui
 
-import androidx.compose.animation.*
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.saurabhsandav.core.trades.model.TradeExecutionId
 import com.saurabhsandav.core.ui.common.*
-import com.saurabhsandav.core.ui.theme.dimens
 
 @Composable
 internal fun TradeExecutionsSelectionBar(
@@ -25,74 +13,44 @@ internal fun TradeExecutionsSelectionBar(
     onDeleteExecutions: (List<TradeExecutionId>) -> Unit,
 ) {
 
-    AnimatedVisibility(
-        visible = selectionManager.selection.isNotEmpty(),
-        enter = fadeIn() + expandVertically(),
-        exit = shrinkVertically() + fadeOut(),
-    ) {
+    SelectionBar(selectionManager) {
 
-        Surface(color = MaterialTheme.colorScheme.tertiaryContainer) {
+        var showLockConfirmationDialog by state { false }
+        var showDeleteConfirmationDialog by state { false }
 
-            Row(
-                modifier = Modifier.height(48.dp).fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.rowHorizontalSpacing),
-            ) {
+        Item(
+            onClick = { showLockConfirmationDialog = true },
+            text = "Lock",
+        )
 
-                var showLockConfirmationDialog by state { false }
-                var showDeleteConfirmationDialog by state { false }
+        Item(
+            onClick = { showDeleteConfirmationDialog = true },
+            text = "Delete",
+        )
 
-                IconButtonWithTooltip(
-                    onClick = selectionManager::clear,
-                    tooltipText = "Clear selection",
-                    content = {
-                        Icon(Icons.Default.Close, contentDescription = "Clear selection")
-                    },
-                )
+        if (showLockConfirmationDialog) {
 
-                VerticalDivider()
+            ConfirmationDialog(
+                text = "Are you sure you want to lock the executions?",
+                onDismiss = { showLockConfirmationDialog = false },
+                onConfirm = {
+                    showLockConfirmationDialog = false
+                    onLockExecutions(selectionManager.selection.toList())
+                    selectionManager.clear()
+                },
+            )
+        }
 
-                Text("${selectionManager.selection.size} item(s) selected")
+        if (showDeleteConfirmationDialog) {
 
-                VerticalDivider()
-
-                TextButton(onClick = { showLockConfirmationDialog = true }) {
-
-                    Text("LOCK")
-                }
-
-                VerticalDivider()
-
-                TextButton(onClick = { showDeleteConfirmationDialog = true }) {
-
-                    Text("DELETE")
-                }
-
-                if (showLockConfirmationDialog) {
-
-                    ConfirmationDialog(
-                        text = "Are you sure you want to lock the executions?",
-                        onDismiss = { showLockConfirmationDialog = false },
-                        onConfirm = {
-                            showLockConfirmationDialog = false
-                            onLockExecutions(selectionManager.selection.toList())
-                            selectionManager.clear()
-                        },
-                    )
-                }
-
-                if (showDeleteConfirmationDialog) {
-
-                    DeleteConfirmationDialog(
-                        subject = "executions",
-                        onDismiss = { showDeleteConfirmationDialog = false },
-                        onConfirm = {
-                            onDeleteExecutions(selectionManager.selection.toList())
-                            selectionManager.clear()
-                        },
-                    )
-                }
-            }
+            DeleteConfirmationDialog(
+                subject = "executions",
+                onDismiss = { showDeleteConfirmationDialog = false },
+                onConfirm = {
+                    onDeleteExecutions(selectionManager.selection.toList())
+                    selectionManager.clear()
+                },
+            )
         }
     }
 }
