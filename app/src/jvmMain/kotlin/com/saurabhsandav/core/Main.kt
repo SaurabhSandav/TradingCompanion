0ab4e7d8d99280852d1b9afe28993c78b3bdef71
Saurabh Sandav <justmailsaurabh@gmail.com>
2024-12-12 16:44:18 +0530

    Fix: App doesn't exit immediately

--- app/src/jvmMain/kotlin/com/saurabhsandav/core/Main.kt
+++ app/src/jvmMain/kotlin/com/saurabhsandav/core/Main.kt
@@ -5,8 +5,16 @@ import com.github.ajalt.clikt.command.main
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.help
 import com.github.ajalt.clikt.parameters.options.option
+import kotlin.system.exitProcess
 
-suspend fun main(args: Array<String>) = Main().main(args)
+suspend fun main(args: Array<String>) {
+    Main().main(args)
+    // Workaround issue that causes app to not exit immediately.
+    // To Reproduce: Open Chart (Any WebView Backend). Exit. App exits after roughly a minute. Sometimes more.
+    // Issue only happens when using Jetbrains Runtime. Narrowed down the issue to coroutine usage in the
+    // ChartsPresenter class. Couldn't narrow it down further.
+    exitProcess(0)
+}
 
 class Main : SuspendingCliktCommand() {
 
