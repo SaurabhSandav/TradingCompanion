9a5a1945177ca8e93e810c95073a42c7e556a045
Saurabh Sandav <justmailsaurabh@gmail.com>
2025-03-26 13:29:42 +0530

    Trade Redesign

--- app/src/jvmMain/kotlin/com/saurabhsandav/core/ui/trade/TradeScreen.kt
+++ app/src/jvmMain/kotlin/com/saurabhsandav/core/ui/trade/TradeScreen.kt
@@ -1,26 +1,36 @@
 package com.saurabhsandav.core.ui.trade
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.BoxWithConstraintsScope
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.WindowPlacement
 import com.saurabhsandav.core.LocalAppGraph
 import com.saurabhsandav.core.ui.common.BoxWithScrollbar
 import com.saurabhsandav.core.ui.common.app.AppWindow
 import com.saurabhsandav.core.ui.common.app.rememberAppWindowState
+import com.saurabhsandav.core.ui.common.thenIf
 import com.saurabhsandav.core.ui.theme.dimens
 import com.saurabhsandav.core.ui.trade.model.TradeEvent.AddNote
 import com.saurabhsandav.core.ui.trade.model.TradeEvent.AddStop
@@ -46,8 +56,9 @@ import com.saurabhsandav.core.ui.trade.ui.Details
 import com.saurabhsandav.core.ui.trade.ui.Excursions
 import com.saurabhsandav.core.ui.trade.ui.ExecutionsTable
 import com.saurabhsandav.core.ui.trade.ui.Notes
-import com.saurabhsandav.core.ui.trade.ui.StopsAndTargets
+import com.saurabhsandav.core.ui.trade.ui.StopsList
 import com.saurabhsandav.core.ui.trade.ui.Tags
+import com.saurabhsandav.core.ui.trade.ui.TargetsList
 import com.saurabhsandav.core.ui.tradecontent.ProfileTradeId
 
 @Composable
@@ -102,16 +113,21 @@ internal fun TradeWindow(
                     onDeleteExecution = { executionId -> state.eventSink(DeleteExecution(executionId)) },
                 )
             },
-            stopsAndTargets = {
+            stops = {
 
-                StopsAndTargets(
+                StopsList(
                     stops = state.stops,
                     stopPreviewer = state.stopPreviewer,
                     onAddStop = { state.eventSink(AddStop(it)) },
                     onDeleteStop = { state.eventSink(DeleteStop(it)) },
                     onSetPrimaryStop = { state.eventSink(SetPrimaryStop(it)) },
+                )
+            },
+            targets = {
+
+                TargetsList(
                     targets = state.targets,
-                    showTargetRValues = state.showTargetRValues,
+                    showRValues = state.showTargetRValues,
                     targetPreviewer = state.targetPreviewer,
                     onAddTarget = { state.eventSink(AddTarget(it)) },
                     onDeleteTarget = { state.eventSink(DeleteTarget(it)) },
@@ -163,7 +179,8 @@ internal fun TradeWindow(
 internal fun TradeScreen(
     details: @Composable () -> Unit,
     executions: @Composable () -> Unit,
-    stopsAndTargets: @Composable () -> Unit,
+    stops: @Composable () -> Unit,
+    targets: @Composable () -> Unit,
     excursions: @Composable () -> Unit,
     tags: @Composable () -> Unit,
     attachments: @Composable () -> Unit,
@@ -172,33 +189,146 @@ internal fun TradeScreen(
 
     Scaffold {
 
+        BoxWithConstraints(Modifier.fillMaxSize()) {
+
+            AdaptiveLayout(
+                modifier = Modifier
+                    .matchParentSize()
+                    .padding(MaterialTheme.dimens.containerPadding),
+                details = details,
+                tags = tags,
+                executions = executions,
+                stops = stops,
+                targets = targets,
+                attachments = attachments,
+                notes = notes,
+                excursions = excursions,
+            )
+        }
+    }
+}
+
+@Composable
+private fun BoxWithConstraintsScope.AdaptiveLayout(
+    modifier: Modifier,
+    details: @Composable () -> Unit,
+    executions: @Composable () -> Unit,
+    stops: @Composable () -> Unit,
+    targets: @Composable () -> Unit,
+    tags: @Composable () -> Unit,
+    attachments: @Composable () -> Unit,
+    notes: @Composable () -> Unit,
+    excursions: @Composable () -> Unit,
+) {
+
+    val is1Pane = maxWidth < 1000.dp
+    val is2Pane = maxWidth >= 1000.dp && maxWidth < 1680.dp
+    val is3Pane = maxWidth >= 1680.dp
+
+    Row(
+        modifier = modifier.matchParentSize(),
+        horizontalArrangement = Arrangement.spacedBy(
+            space = MaterialTheme.dimens.rowHorizontalSpacing,
+            alignment = Alignment.CenterHorizontally,
+        ),
+    ) {
+
         val scrollState = rememberScrollState()
 
         BoxWithScrollbar(
-            modifier = Modifier.fillMaxSize(),
-            scrollbarAdapter = rememberScrollbarAdapter(scrollState),
+            modifier = Modifier.fillMaxHeight().wrapContentHeight(),
+            scrollbarAdapter = if (is2Pane || is3Pane) null else rememberScrollbarAdapter(scrollState),
         ) {
 
             Column(
                 modifier = Modifier
-                    .verticalScroll(scrollState)
-                    .padding(MaterialTheme.dimens.containerPadding),
+                    .thenIf(
+                        condition = is1Pane,
+                        ifFalse = { width(400.dp) },
+                        ifTrue = { matchParentSize() },
+                    )
+                    .verticalScroll(scrollState),
                 verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.columnVerticalSpacing),
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
 
                 details()
-
-                executions()
-
-                stopsAndTargets()
-
-                excursions()
-
                 tags()
 
-                attachments()
+                if (is1Pane) {
+                    executions()
+                    stops()
+                    targets()
+                    tags()
+                    excursions()
+                    attachments()
+                    notes()
+                }
+            }
+        }
 
-                notes()
+        if (is2Pane || is3Pane) {
+
+            VerticalDivider()
+
+            val scrollStatePane2 = rememberScrollState()
+
+            BoxWithScrollbar(
+                modifier = Modifier
+                    .thenIf(
+                        condition = is2Pane,
+                        ifFalse = { width(700.dp) },
+                        ifTrue = { weight(1F) },
+                    )
+                    .fillMaxHeight(),
+                scrollbarAdapter = rememberScrollbarAdapter(scrollStatePane2),
+            ) {
+
+                Column(
+                    modifier = Modifier.verticalScroll(scrollStatePane2),
+                    verticalArrangement = Arrangement.spacedBy(
+                        space = MaterialTheme.dimens.columnVerticalSpacing,
+                        alignment = Alignment.CenterVertically,
+                    ),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+
+                    executions()
+                    stops()
+                    targets()
+
+                    if (is2Pane) {
+                        excursions()
+                        attachments()
+                        notes()
+                    }
+                }
+            }
+        }
+
+        if (is3Pane) {
+
+            VerticalDivider()
+
+            val scrollStatePane3 = rememberScrollState()
+
+            BoxWithScrollbar(
+                modifier = Modifier.weight(1F).fillMaxHeight(),
+                scrollbarAdapter = rememberScrollbarAdapter(scrollStatePane3),
+            ) {
+
+                Column(
+                    modifier = Modifier.verticalScroll(scrollStatePane3),
+                    verticalArrangement = Arrangement.spacedBy(
+                        space = MaterialTheme.dimens.columnVerticalSpacing,
+                        alignment = Alignment.CenterVertically,
+                    ),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    excursions()
+                    attachments()
+                    notes()
+                }
             }
         }
     }
--- app/src/jvmMain/kotlin/com/saurabhsandav/core/ui/trade/ui/StopsAndTargets.kt
+++ app/src/jvmMain/kotlin/com/saurabhsandav/core/ui/trade/ui/StopsAndTargets.kt
@@ -1,68 +1,17 @@
 package com.saurabhsandav.core.ui.trade.ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.outlined.CheckCircleOutline
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.saurabhsandav.core.ui.common.DeleteConfirmationDialog
 import com.saurabhsandav.core.ui.common.IconButtonWithTooltip
 import com.saurabhsandav.core.ui.common.state
-import com.saurabhsandav.core.ui.theme.dimens
-import com.saurabhsandav.core.ui.trade.StopPreviewer
-import com.saurabhsandav.core.ui.trade.TargetPreviewer
-import com.saurabhsandav.core.ui.trade.model.TradeState.TradeStop
-import com.saurabhsandav.core.ui.trade.model.TradeState.TradeTarget
-import com.saurabhsandav.kbigdecimal.KBigDecimal
-import kotlinx.coroutines.flow.Flow
-
-@Composable
-internal fun StopsAndTargets(
-    stops: List<TradeStop>,
-    stopPreviewer: Flow<StopPreviewer>,
-    onAddStop: (KBigDecimal) -> Unit,
-    onDeleteStop: (KBigDecimal) -> Unit,
-    onSetPrimaryStop: (KBigDecimal) -> Unit,
-    targets: List<TradeTarget>,
-    showTargetRValues: Boolean,
-    targetPreviewer: Flow<TargetPreviewer>,
-    onAddTarget: (KBigDecimal) -> Unit,
-    onDeleteTarget: (KBigDecimal) -> Unit,
-    onSetPrimaryTarget: (KBigDecimal) -> Unit,
-) {
-
-    Row(
-        horizontalArrangement = Arrangement.spacedBy(MaterialTheme.dimens.rowHorizontalSpacing),
-    ) {
-
-        StopsList(
-            modifier = Modifier.weight(1F),
-            stops = stops,
-            stopPreviewer = stopPreviewer,
-            onAddStop = onAddStop,
-            onDeleteStop = onDeleteStop,
-            onSetPrimaryStop = onSetPrimaryStop,
-        )
-
-        TargetsList(
-            modifier = Modifier.weight(1F),
-            targets = targets,
-            showRValues = showTargetRValues,
-            targetPreviewer = targetPreviewer,
-            onAddTarget = onAddTarget,
-            onDeleteTarget = onDeleteTarget,
-            onSetPrimaryTarget = onSetPrimaryTarget,
-        )
-    }
-}
 
 @Composable
 internal fun DeleteIconButton(
