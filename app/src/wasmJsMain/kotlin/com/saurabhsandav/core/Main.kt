a362a1e7de356fce15a35ae98dd965b511095613
Saurabh Sandav <justmailsaurabh@gmail.com>
2025-06-07 18:38:03 +0530

    Add WASM support

---
+++ app/src/wasmJsMain/kotlin/com/saurabhsandav/core/Main.kt
@@ -0,0 +1,18 @@
+package com.saurabhsandav.core
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+import kotlinx.browser.document
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+
+    ComposeViewport(document.body!!) {
+
+        MaterialTheme {
+            Text("Hello World!")
+        }
+    }
+}
