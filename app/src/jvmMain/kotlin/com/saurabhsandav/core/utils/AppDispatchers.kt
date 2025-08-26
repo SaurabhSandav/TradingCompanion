a8c1284f304983ccb2b47b8fed01355a8e15174a
Saurabh Sandav <justmailsaurabh@gmail.com>
2025-08-26 15:51:41 +0530

    AppDispatchers: Change type of parameters to CoroutineContext

--- app/src/jvmMain/kotlin/com/saurabhsandav/core/utils/AppDispatchers.kt
+++ app/src/jvmMain/kotlin/com/saurabhsandav/core/utils/AppDispatchers.kt
@@ -1,11 +1,11 @@
 package com.saurabhsandav.core.utils
 
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlin.coroutines.CoroutineContext
 
 class AppDispatchers(
-    val Main: CoroutineDispatcher = Dispatchers.Main,
-    val Default: CoroutineDispatcher = Dispatchers.Default,
-    val IO: CoroutineDispatcher = Dispatchers.IO,
-    val Unconfined: CoroutineDispatcher = Dispatchers.Unconfined,
+    val Main: CoroutineContext = Dispatchers.Main,
+    val Default: CoroutineContext = Dispatchers.Default,
+    val IO: CoroutineContext = Dispatchers.IO,
+    val Unconfined: CoroutineContext = Dispatchers.Unconfined,
 )
